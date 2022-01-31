b6e05ad8ab48177171a5e3a9de22f9380cbe673f
Eleonora Hwang <eleonorahwang@gmail.com>
2022-01-31 11:44:22 -0800

    completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,46 @@
+var width = 5;
+var height = 2;
+var area = width * height;
+
+console.log('value of area: ', area);
+console.log('typeof area: ', typeof area);
+
+var bill = 100;
+var payment = 120;
+var change = payment - bill;
+
+console.log('value of change: ', change);
+console.log('typeof change: ', typeof change);
+
+var quizzes = 50;
+var midterm = 60;
+var final = 70;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade: ', grade);
+console.log('typeof grade: ', typeof grade);
+
+var firstName = 'Ellie';
+var lastName = 'Hwang';
+var fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName: ', fullName);
+console.log('typeof fullName: ', typeof fullName);
+
+var pH = 5;
+var isAcidic = pH < 7;
+
+console.log('value of isAcidic: ', isAcidic);
+console.log('typeof isAcidic: ', typeof isAcidic);
+
+var headCount = 20;
+var isSparta = headCount === 300;
+
+console.log('value of isSparta: ', isSparta);
+console.log('typeof isSparta: ', typeof isSparta);
+
+var motto = fullName;
+motto = motto + ' is the GOAT';
+
+console.log('value of motto: ', motto);
+console.log('typeof motto: ', typeof motto);
