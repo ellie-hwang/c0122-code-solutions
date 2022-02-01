67fb70a4320063d427d9c3665e6ac69117ca6b3e
Eleonora Hwang <eleonorahwang@gmail.com>
2022-02-01 11:53:46 -0800

    added itemType property and put items in array

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,10 +1,14 @@
 var orderHistory = [
   {
-    book: {
-      title: 'Javascript for Impatient programmers',
-      author: 'Dr. Axel Rauschmayer'
-    },
-    itemImg: true,
+    items: [
+      {
+        itemType: 'book',
+        title: 'Javascript for Impatient programmers',
+        author: 'Dr. Axel Rauschmayer',
+        itemImg: true,
+        price: 31.55
+      }
+    ],
     orderDate: 'August 4, 2020',
     orderStatus:
     {
@@ -12,14 +16,18 @@ var orderHistory = [
       deliveryDate: 'August 8, 2020',
       returnCloseDate: 'September 7, 2020'
     },
-    price: 31.55
+    price: 34
   },
   {
-    book: {
-      title: 'The Timeless Way of Building',
-      author: 'Christopher Alexander'
-    },
-    itemImg: true,
+    items: [
+      {
+        itemType: 'book',
+        title: 'The Timeless Way of Building',
+        author: 'Christopher Alexander',
+        itemImg: true,
+        price: 41.33
+      }
+    ],
     orderDate: 'July 19, 2020',
     orderStatus:
     {
@@ -27,13 +35,17 @@ var orderHistory = [
       deliveryDate: 'July 20, 2020',
       returnCloseDate: 'August 19, 2020'
     },
-    price: 41.33
+    totalPrice: 44.53
   },
   {
-    electronics: {
-      itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No lag-Gamecube Adapter'
-    },
-    itemImg: true,
+    items: [
+      {
+        itemType: 'electronics',
+        itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No lag-Gamecube Adapter',
+        itemImg: true,
+        price: 15.98
+      }
+    ],
     orderDate: 'July 4, 2020',
     orderStatus:
     {
@@ -41,13 +53,25 @@ var orderHistory = [
       deliveryDate: 'July 7, 2020',
       returnCloseDate: 'August 5, 2020'
     },
-    price: 15.98
+    totalPrice: 17.22
   },
+
   {
-    electronics: {
-      itemName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)'
-    },
-    itemImg: true,
+    items: [
+      {
+        itemType: 'electronics',
+        itemName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        itemImg: true,
+        price: 94.95
+      },
+      {
+        itemType: 'book',
+        title: 'The Art of Sql',
+        author: 'Stephane Faroult',
+        itemImg: true,
+        price: 33.99
+      }
+    ],
     orderDate: 'July 3, 2020',
     orderStatus:
     {
@@ -55,24 +79,8 @@ var orderHistory = [
       deliveryDate: 'July 5, 2020',
       returnCloseDate: 'August 4, 2020'
     },
-    price: 94.95
-  },
-  {
-    book: {
-      title: 'The Art of Sql',
-      author: 'Stephane Faroult'
-    },
-    itemImg: true,
-    orderDate: 'July 3, 2020',
-    orderStatus:
-    {
-      deliveryStatus: true,
-      deliveryDate: 'July 5, 2020',
-      returnCloseDate: 'August 4, 2020'
-    }
+    totalPrice: 138.93
   }
 ];
 
-console.log('Title of book ordered on August 4, 2020:', orderHistory[0].book.title);
-console.log('Delivery date for Gamecube Controller: ', orderHistory[3].orderStatus.deliveryDate);
 console.log('Order History: ', orderHistory);
