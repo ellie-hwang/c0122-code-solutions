2a4d0ac998c5cd6ce1dbbf8f243a0be14c86d271
Eleonora Hwang <eleonorahwang@gmail.com>
2022-02-08 16:20:39 -0800

    added comments, fixed typo

--- javascript-forms/form-events.js
+++ javascript-forms/form-events.js
@@ -1,22 +1,23 @@
 function handleFocus(event) {
   console.log('Focus event fired');
-  console.log(event.target);
+  console.log(event.target.name);
 }
 
 function handleBlur(event) {
-  console.log('Blur even fired');
-  console.log(event.target);
+  console.log('Blur eventinpu fired');
+  console.log(event.target.name);
 }
 
 function handleInput(event) {
   console.log('Input event fired');
-  console.log(event.target);
+  console.log(event.target.name);
   console.log(event.target.value);
 }
 
 var $userName = document.querySelector('input[name= "name"]');
 var $email = document.querySelector('input[name="email"]');
 var $message = document.querySelector('textarea[name="message"]');
+// var $message = document.querySelector('#user-message');
 
 $userName.addEventListener('focus', handleFocus);
 $userName.addEventListener('blur', handleBlur);
