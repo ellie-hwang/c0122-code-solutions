4cb7fbe817d5ccd158387742acf6534719018fe1
Eleonora Hwang <eleonorahwang@gmail.com>
2022-02-10 15:24:27 -0800

    completed dom-event-delegation

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,12 @@
+var $taskList = document.querySelector('.task-list');
+$taskList.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  console.log('event.target: ', event.target);
+  console.log('event.target.tagName: ', event.target.tagName);
+  if (event.target && event.target.tagName === 'BUTTON') {
+    var $liEl = event.target.closest('.task-list-item');
+    console.log('closest .task-list-item: ', $liEl);
+    $liEl.remove();
+  }
+}
