df9e65b5a13b345b9a89a4b80f95e853736ec080
Eleonora Hwang <eleonorahwang@gmail.com>
2022-02-12 21:49:31 -0800

    save point

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,22 @@
 /* exported invert */
+
+function invert(source) {
+  var invertObj = {};
+  var invertValues = Object.values(source);
+  // console.log(invertValues);
+  var invertKeys = Object.keys(source);
+  // console.log(invertKeys);
+  for (let i = 0; i < Object.keys(source).length; i++) {
+    invertObj[invertKeys[i]] = invertValues[i];
+  }
+  // console.log(invertObj);
+  return invertObj;
+}
+
+var source = {
+  age: 'bloop',
+  occupation: 'programmer',
+  language: 'JavaScript'
+};
+
+invert(source);
--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,18 @@
 /* exported omit */
+/* function: to return an object with the omitted keys and key values that are NOT in the
+keys paramter
+input: object and keys
+output: object
+*/
+
+function omit(source, keys) {
+  var omitObj = { ...source };
+  // debugger;
+
+  for (let i = 0; i < keys.length; i++) {
+    if (keys[i] in omitObj) {
+      delete omitObj[keys[i]];
+    }
+  }
+  return omitObj;
+}
