a41fae6d8ea6405144238282f1d5736ba9107314
Eleonora Hwang <eleonorahwang@gmail.com>
2022-02-16 18:05:17 -0800

    Worked on zip and flatten

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,15 @@
 /* exported flatten */
+
+function flatten(array) {
+  var flatArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].length !== undefined) {
+      for (let j = 0; j < array[i].length; j++) {
+        flatArray.push(array[i][j]);
+      }
+    } else if (array[i].length === undefined) {
+      flatArray.push(array[i]);
+    }
+  } return flatArray;
+}
--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,15 @@
 /* exported zip */
+
+function zip(first, second) {
+  var zipArray = [];
+  var currentArray = [];
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] !== undefined && second[i] !== undefined) {
+      currentArray.push(first[i]);
+      currentArray.push(second[i]);
+      zipArray.push(currentArray);
+      currentArray = [];
+    }
+  }
+  return zipArray;
+}
