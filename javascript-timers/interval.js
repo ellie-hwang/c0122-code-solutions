552b74a78132f28de574b9d05da6481c2e82bef4
Eleonora Hwang <eleonorahwang@gmail.com>
2022-02-23 11:37:01 -0800

    Added  variable to account for counts starting at higher numbers

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,15 +1,12 @@
 var $countdownDisplay = document.querySelector('.countdown-display');
+var $startingNumber = parseInt($countdownDisplay.textContent);
 
 function countdownDisplay() {
-  if ($countdownDisplay.textContent === '4') {
-    $countdownDisplay.textContent = '3';
-  } else if ($countdownDisplay.textContent === '3') {
-    $countdownDisplay.textContent = '2';
-  } else if ($countdownDisplay.textContent === '2') {
-    $countdownDisplay.textContent = '1';
-  } else if ($countdownDisplay.textContent === '1') {
+  if ($startingNumber !== 1) {
+    $startingNumber--;
+    $countdownDisplay.textContent = $startingNumber;
+  } else if ($startingNumber === 1) {
     $countdownDisplay.textContent = '~Earth Beeeelooowww Us~';
-  } else if ($countdownDisplay.textContent === '~Earth Beeeelooowww Us~') {
     clearInterval(intervId);
   }
 }
