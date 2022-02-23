c6fd92e30d50c5f74a049a30fa015192f27e8954
Eleonora Hwang <eleonorahwang@gmail.com>
2022-02-23 14:33:21 -0800

    Completed javascript-carousel

---
+++ javascript-carousel/main.js
@@ -0,0 +1,59 @@
+var $carouselImg = document.querySelectorAll('.carousel-img');
+var $carouselPosition = document.querySelectorAll('.carousel-position');
+var $container = document.querySelector('.container');
+
+$container.addEventListener('click', switchImage);
+
+var timerId = null;
+var i = 0;
+
+function switchImage(event) {
+  if (event.target.getAttribute('class') === 'fa-solid fa-angle-right resize-big') {
+    if (i < $carouselImg.length - 1) {
+      $carouselImg[i].className = 'carousel-img hidden';
+      $carouselImg[i + 1].className = 'carousel-img';
+      $carouselPosition[i].className = 'fa-regular fa-circle carousel-position';
+      $carouselPosition[i + 1].className = 'fa-solid fa-circle carousel-position';
+      i++;
+    } else if (i === $carouselImg.length - 1) {
+      $carouselImg[i].className = 'carousel-img hidden';
+      $carouselPosition[i].className = 'fa-regular fa-circle carousel-position';
+      i = 0;
+      $carouselImg[i].className = 'carousel-img';
+      $carouselPosition[i].className = 'fa-solid fa-circle carousel-position';
+    }
+  } else if (event.target.getAttribute('class') === 'fa-solid fa-angle-left resize-big') {
+    if (i < $carouselImg.length && i !== 0) {
+      $carouselImg[i].className = 'carousel-img hidden';
+      $carouselImg[i - 1].className = 'carousel-img';
+      $carouselPosition[i].className = 'fa-regular fa-circle carousel-position';
+      $carouselPosition[i - 1].className = 'fa-solid fa-circle carousel-position';
+      i--;
+    } else if (i === 0) {
+      $carouselImg[i].className = 'carousel-img hidden';
+      $carouselPosition[i].className = 'fa-regular fa-circle carousel-position';
+      i = $carouselImg.length - 1;
+      $carouselImg[i].className = 'carousel-img';
+      $carouselPosition[i].className = 'fa-solid fa-circle carousel-position';
+    }
+  }
+}
+
+function autoSwitchImage() {
+  if (i < $carouselImg.length - 1) {
+    $carouselImg[i].className = 'carousel-img hidden';
+    $carouselImg[i + 1].className = 'carousel-img';
+    $carouselPosition[i].className = 'fa-regular fa-circle carousel-position';
+    $carouselPosition[i + 1].className = 'fa-solid fa-circle carousel-position';
+    i++;
+  } else if (i === $carouselImg.length - 1) {
+    $carouselImg[i].className = 'carousel-img hidden';
+    $carouselPosition[i].className = 'fa-regular fa-circle carousel-position';
+    i = 0;
+    $carouselImg[i].className = 'carousel-img';
+    $carouselPosition[i].className = 'fa-solid fa-circle carousel-position';
+    clearInterval(timerId);
+  }
+}
+
+timerId = setInterval(autoSwitchImage, 3000);
