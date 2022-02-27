3ed1f1db526103a0d66ce3c6eeb878870e271b12
Eleonora Hwang <eleonorahwang@gmail.com>
2022-02-26 22:17:31 -0800

    Set up setRequestHeader for XMLHttpRequest object

--- testing-apis/movies.js
+++ testing-apis/movies.js
@@ -1,12 +1,15 @@
 function getMovieData() {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://betterimdbot.herokuapp.com/swagger.json');
+  xhr.open('POST', 'https://betterimdbot.herokuapp.com/swagger.json');
+  xhr.setRequestHeader('accept', 'application/json');
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     console.log(xhr.status);
     console.log(xhr.response);
   });
-  xhr.send();
+  var param = 'q=q%20-%20twilight';
+  xhr.send(param);
 }
 
 getMovieData();
