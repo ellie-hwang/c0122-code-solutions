7d433f082175e19b6d231a348fd0a2fa62b330d3
Eleonora Hwang <eleonorahwang@gmail.com>
2022-02-28 18:01:49 -0800

    Testing api's some more

--- testing-apis/movies.js
+++ testing-apis/movies.js
@@ -1,6 +1,6 @@
 function getMovieData() {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://www.omdbapi.com/?t=twilight&apikey=fd3f5e28');
+  xhr.open('GET', 'https://www.omdbapi.com/?t=spongebob+movie&apikey=fd3f5e28');
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     console.log(xhr.status);
