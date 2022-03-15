0ebd518c036368ccdf40dd82a5791b9e209a9c6b
Eleonora Hwang <eleonorahwang@gmail.com>
2022-03-15 12:27:34 -0700

    Converted setTimeout callbacks to arrow functions. Removed _this variable and converted renderJokePhrase and appendJokePhrase back to es5 function definitions

--- es6-arrow-functions/main.js
+++ es6-arrow-functions/main.js
@@ -5,26 +5,25 @@ const $jokeForm = document.querySelector('#joke-form');
 const jokester = {
   tellJoke: function (setup, punchline) {
     $jokeForm.classList.add('d-none');
-    const this_ = this;
-    const $introStatement = this_.renderJokePhrase('Hey Flash...');
-    this_.appendJokePhrase($introStatement);
-    setTimeout(function () {
-      const $jokeSetup = this_.renderJokePhrase(setup);
-      this_.appendJokePhrase($jokeSetup);
-      setTimeout(function () {
-        const $jokePunchline = this_.renderJokePhrase(punchline);
-        this_.appendJokePhrase($jokePunchline);
+    const $introStatement = this.renderJokePhrase('Hey Flash...');
+    this.appendJokePhrase($introStatement);
+    setTimeout(() => {
+      const $jokeSetup = this.renderJokePhrase(setup);
+      this.appendJokePhrase($jokeSetup);
+      setTimeout(() => {
+        const $jokePunchline = this.renderJokePhrase(punchline);
+        this.appendJokePhrase($jokePunchline);
         flash.laugh();
       }, 2000);
     }, 2000);
   },
-  renderJokePhrase: phrase => {
+  renderJokePhrase: function (phrase) {
     const $phrase = document.createElement('h4');
     $phrase.textContent = phrase;
     $phrase.className = 'fade-in text-center';
     return $phrase;
   },
-  appendJokePhrase: $phrase => {
+  appendJokePhrase: function ($phrase) {
     $jokeContainer.append($phrase);
   }
 };
@@ -32,9 +31,8 @@ const jokester = {
 const flash = {
   laughingUrl: 'images/flash-laugh.gif',
   laugh: function () {
-    const this_ = this;
-    setTimeout(function () {
-      $flashImage.setAttribute('src', this_.laughingUrl);
+    setTimeout(() => {
+      $flashImage.setAttribute('src', this.laughingUrl);
     }, 3000);
   }
 };
