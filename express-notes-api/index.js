88e1c4a8300d96362a9467a9889ff83600e02e67
Eleonora Hwang <eleonorahwang@gmail.com>
2022-03-21 14:30:56 -0700

    Feature 2: Client receives 400 status response if request target id is not a positive integer

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -8,6 +8,10 @@ app.listen(3000, () => {
   console.log('Listening on port 3000!');
 });
 
+const jsonParse = express.json();
+
+app.use('/api/notes', jsonParse);
+
 app.get('/api/notes', (req, res) => {
   const gradesArray = [];
   fs.readFile('data.json', 'utf8', (err, data) => {
@@ -21,4 +25,12 @@ app.get('/api/notes', (req, res) => {
     }
     res.json(gradesArray);
   });
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  // const notes = JSONdata.notes;
+  if (!parseInt(req.params.id) === true) {
+    const errorObj = { error: 'id must be a positive integer' };
+    res.status(400).send(errorObj);
+  }
 });
