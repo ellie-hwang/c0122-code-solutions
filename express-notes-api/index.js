5ffd38e892b72a7553a4e5692e688a4d2170685f
Eleonora Hwang <eleonorahwang@gmail.com>
2022-03-21 18:21:17 -0700

    Completed Feature 5: client can update a note by id and with content

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -78,7 +78,7 @@ app.delete('/api/notes/:id', (req, res) => {
   } else if (JSONdata.notes[notesId] !== undefined) {
     delete JSONdata.notes[notesId];
     const newJSONdata = JSON.stringify(JSONdata, null, 2);
-    fs.writeFile('derp/data.json', newJSONdata, 'utf8', err => {
+    fs.writeFile('data.json', newJSONdata, 'utf8', err => {
       if (err) {
         console.error(err);
         const errorObj = { error: 'An unexpected error occurred.' };
@@ -86,6 +86,33 @@ app.delete('/api/notes/:id', (req, res) => {
         process.exit(1);
       }
       res.sendStatus(204);
+    });
+  }
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  const notesId = req.params.id;
+  if (!parseInt(notesId) === true) {
+    if (req.body === {} || req.body.content === undefined || req.body.content === '') {
+      const errorObj = { error: 'content is a required field' };
+      res.status(400).send(errorObj);
+    }
+    const errorObj = { error: 'id must be a positive integer' };
+    res.status(400).send(errorObj);
+  } else if (JSONdata.notes[notesId] === undefined) {
+    const errorObj = { error: 'cannot find note with id ' + notesId };
+    res.status(404).send(errorObj);
+  } else if (JSONdata.notes[notesId] !== undefined && req.body.content !== undefined && req.body.content !== {} && req.body.content !== '') {
+    JSONdata.notes[notesId].content = req.body.content;
+    const newJSONdata = JSON.stringify(JSONdata, null, 2);
+    fs.writeFile('data.json', newJSONdata, 'utf8', err => {
+      if (err) {
+        console.error(err);
+        const errorObj = { error: 'An unexpected error occurred.' };
+        res.status(500).send(errorObj);
+        process.exit(1);
+      }
+      res.status(200).send(JSONdata.notes[notesId]);
     });
   }
 });
