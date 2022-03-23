8c35513dd627e71e3a995d4c96f1db0bced86a54
Eleonora Hwang <eleonorahwang@gmail.com>
2022-03-23 13:11:20 -0700

    Completed magic-mallrats

---
+++ sql-join/where-are-they.sql
@@ -0,0 +1,4 @@
+select "addresses"."line1", "cities"."name" as "city", "addresses"."district", "countries"."name" as "country"
+  from "addresses"
+  join "cities" using ("cityId")
+  join "countries" using ("countryId")
