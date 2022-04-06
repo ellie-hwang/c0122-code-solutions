e2773cc3c72fccc6081f41e86bab5a42e0e574bc
Eleonora Hwang <eleonorahwang@gmail.com>
2022-04-06 00:09:39 -0700

    Removed showDetails and hideDetails methods. Stored topic.id in data-topic-id prop. Added conditions withing anonymous arrow function within the topics.map method.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -4,19 +4,16 @@ const topics = [
   {
     id: 1,
     topic: 'Tastea',
-    heading: 'Tastea',
     details: 'They have solid fruit teas and their boba is green. Their milk teas are not very tasty.Popcorn chicken and fries are great, but the portion size got smaller.'
   },
   {
     id: 2,
-    topic: 'HappyLemon',
-    heading: 'Happy Lemon',
+    topic: 'Happy Lemon',
     details: 'Don\'t get their boba. They specialize in cheese foam drinks. They also have bubble waffles which are pretty good.Sometimes they have crepecakes too.'
   },
   {
     id: 3,
-    topic: 'SevenLeaves',
-    heading: '7 Leaves',
+    topic: '7 Leaves',
     details: 'Boba is a hit or a miss.Sometimes it\'s too squishy and overcooked. Their taro milk tea is the best, but it could do with less sugar. Their menu has a good variety of teas.'
   }
 ];
@@ -25,56 +22,50 @@ class Accordion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      topic: '',
-      display: false
+      topicId: null,
+      isOpen: false
     };
     this.handleClick = this.handleClick.bind(this);
-    this.hideDetails = this.hideDetails.bind(this);
-    this.showDetails = this.showDetails.bind(this);
-  }
-
-  hideDetails() {
-    const details = document.querySelector('#' + this.state.topic);
-    details.classList.add('display-none');
-    const newTopic = '';
-    this.setState({
-      topic: newTopic
-    });
-  }
-
-  showDetails() {
-    const details = document.querySelector('#' + this.state.topic);
-    details.classList.remove('display-none');
   }
 
   handleClick(event) {
-    if (event.target.getAttribute('topic') === this.state.topic) {
-      const newDisplay = false;
+    if (parseInt(event.target.getAttribute('data-topic-id')) === this.state.topicId) {
       this.setState({
-        display: newDisplay
-      }, this.hideDetails);
-    } else if (event.target.getAttribute('topic') !== this.state.topic && this.state.display === false) {
-      const newDisplay = true;
+        topicId: null,
+        isOpen: false
+      });
+    } else if (parseInt(event.target.getAttribute('data-topic-id')) !== this.state.topicId && this.state.isOpen === true) {
       this.setState({
-        topic: event.target.getAttribute('topic'),
-        display: newDisplay
-      }, this.showDetails);
-    } else if (event.target.getAttribute('topic') !== this.state.topic && this.state.display === true) {
-      const newDisplay = true;
-      this.hideDetails();
+        topicId: parseInt(event.target.getAttribute('data-topic-id')),
+        isOpen: true
+      });
+    } else if (parseInt(event.target.getAttribute('data-topic-id')) !== this.state.topicId && this.state.isOpen === false) {
       this.setState({
-        topic: event.target.getAttribute('topic'),
-        display: newDisplay
-      }, this.showDetails);
+        topicId: parseInt(event.target.getAttribute('data-topic-id')),
+        isOpen: true
+      });
     }
   }
 
   render() {
-    const tabItems = topics.map(topic =>
-      <div className="accordion-tab" key={topic.id.toString()}>
-        <p className="tab-header" onClick={this.handleClick} topic={topic.topic}>{topic.heading}</p>
-        <p className="content-text display-none" id={topic.topic}>{topic.details}</p>
-      </div>);
+    const className = this.state.isOpen ? '' : 'display-none';
+    const tabItems = topics.map(topic => {
+      if (topic.id === this.state.topicId) {
+        return (
+        <div className="accordion-tab" key={topic.id.toString()}>
+          <h2 className="tab-header" onClick={this.handleClick} data-topic-id={topic.id}>{topic.topic}</h2>
+          <p className={`content-text ${className}`}>{topic.details}</p>
+        </div>
+        );
+      } else {
+        return (
+        <div className="accordion-tab" key={topic.id.toString()}>
+          <h2 className="tab-header" onClick={this.handleClick} data-topic-id={topic.id}>{topic.topic}</h2>
+            <p className="content-text display-none">{topic.details}</p>
+        </div>
+        );
+      }
+    });
     return (
       <div className="accordion-container">
         {tabItems}
