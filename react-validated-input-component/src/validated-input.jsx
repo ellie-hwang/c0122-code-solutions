8ec51f553d6bc0794370589893c3e13e828c8a86
Eleonora Hwang <eleonorahwang@gmail.com>
2022-04-11 23:57:51 -0700

    Completed react-validated-input-component

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -16,12 +16,13 @@ class ValidatedInput extends React.Component {
   render() {
     const reqPass = this.state.password ? 'display-none' : '';
     const less8 = (this.state.password && this.state.password.length < 8) ? '' : 'display-none';
+    const modClass = this.state.password.length < 8 ? 'fas fa-times' : 'fas fa-check';
     return (
       <div className="row">
         <form>
           <label htmlFor="pass">Password</label>
           <input type="password" name="password" id="pass" onChange={this.handleChange} />
-          <i className="fas fa-times"></i>
+          <i className={`${modClass}`}></i>
           <p className={`message ${reqPass}`}>A password is required</p>
           <p className={`message ${less8}`}>Your password is too short</p>
         </form>
