9173b89322883bb5b509060d0eabdbef224e3e72
Eleonora Hwang <eleonorahwang@gmail.com>
2022-04-26 21:33:07 -0700

    Removed arrays from get-2nd-from-top.js, insert-under-top.js, max-value.js

--- data-structures-stacks/get-2nd-from-top.js
+++ data-structures-stacks/get-2nd-from-top.js
@@ -1,16 +1,14 @@
 /* exported get2ndFromTop */
 
 function get2ndFromTop(stack) {
-  const stackValues = [];
-  while (stack.peek() !== undefined) {
-    stackValues.push(stack.pop());
-  }
-  for (let i = stackValues.length - 1; i >= 0; i--) {
-    stack.push(stackValues[i]);
-  }
-  if (stackValues.length <= 1) {
+  if (stack.peek() === undefined) return;
+  const top = stack.pop();
+  if (stack.peek() === undefined) {
+    stack.push(top);
     return undefined;
   } else {
-    return stackValues[1];
+    const second = stack.peek();
+    stack.push(top);
+    return second;
   }
 }
--- data-structures-stacks/insert-under-top.js
+++ data-structures-stacks/insert-under-top.js
@@ -4,13 +4,7 @@ function insertUnderTop(stack, value) {
   if (stack.peek() === undefined) {
     return;
   }
-  const stackValues = [];
-  while (stack.peek() !== undefined) {
-    stackValues.push(stack.pop());
-  }
-  stackValues.splice(1, 0, value);
-  for (let i = stackValues.length - 1; i >= 0; i--) {
-    stack.push(stackValues[i]);
-  }
-  return stack;
+  const top = stack.pop();
+  stack.push(value);
+  stack.push(top);
 }
